36e2f52b082cf956ec07152317715a8dc20cbdc2
Salvatore <workstudioms@gmail.com>
2025-02-04 16:39:49 +0100

    posts images url, fix

--- app.js
+++ app.js
@@ -8,41 +8,48 @@ app.get('/', (req, res) => {
 
 })
 
+app.use(express.static('public'));
+
 app.get('/bacheca', (req, res) => {
 
     const bacheca = [
         {
+
             titolo: 'Post ciambellone',
             contenuto: 'Dolci',
-            immagine: "http://localhost:300/ciambellone.jpeg",
+            immagine: "http://localhost:3000/postpiatti/ciambellone.jpeg",
             tags: ['DolceFattoInCasa', 'RicettaFacile', 'DolciItaliani']
 
         },
         {
+
             titolo: 'Post cracker barbabietola',
             contenuto: 'Snack',
-            immagine: "http://localhost:300/cracker_barbabietola.jpeg",
+            immagine: "http://localhost:3000/postpiatti/cracker_barbabietola.jpeg",
             tags: ['SnackSalutari', 'SnackAlternativi', 'RicettaVegana']
 
         },
         {
+
             titolo: 'Post pane fritto dolce',
             contenuto: 'Dolci',
-            immagine: "http://localhost:300/pane_fritto_dolce.jpeg",
+            immagine: "http://localhost:3000/postpiatti/pane_fritto_dolce.jpeg",
             tags: ['DolceFattoInCasa', 'RicettaFacile', 'DolciItaliani']
 
         },
         {
+
             titolo: 'Post pasta alla barbabietola',
             contenuto: 'Primi piatti',
-            immagine: "http://localhost:300/pasta_barbabietola.jpeg",
+            immagine: "http://localhost:3000/postpiatti/pasta_barbabietola.jpeg",
             tags: ['CucinaCreativa', 'RicettaVegana', 'PranzoGourmet']
 
         },
         {
+
             titolo: 'Post torta paesana',
             contenuto: 'Dolci',
-            immagine: "http://localhost:300/torta_paesana.jpeg",
+            immagine: "http://localhost:3000/postpiatti/torta_paesana.jpeg",
             tags: ['DolceFattoInCasa', 'RicettaFacile', 'DolciItaliani']
 
         },
